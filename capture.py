26a409b71f79ada1535efb40e8d8fa1f9d67d44e
matthaeus123 <60934578+matthaeus123@users.noreply.github.com>
2023-03-20 15:23:05 -0700

    Update capture.py
    made sure that code works continually

--- capture.py
+++ capture.py
@@ -1,6 +1,6 @@
 import subprocess
 import time
-from gpiozero import MotionSensor, DistanceSensor, LED, OutputDevice
+from gpiozero import MotionSensor, DistanceSensor, LED
 
 # Initialize the motion sensor
 pir = MotionSensor(4)
@@ -11,15 +11,8 @@ ultrasonic = DistanceSensor(echo=4, trigger=5)
 # Initialize the LED
 led = LED(17)
 
-# Initialize the flash trigger
-flash = OutputDevice(27, active_high=False)
-
-# Define function to take picture and trigger flash
-def take_picture_and_flash():
-    # Trigger the flash
-    flash.on()
-    time.sleep(0.05)  # Flash duration
-    flash.off()
+# Define function to take picture
+def take_picture():
     # Take the picture
     led.on()  # Turn on the LED
     subprocess.call(["gphoto2", "--trigger-capture"])
@@ -36,14 +29,12 @@ if __name__ == '__main__':
             distance = ultrasonic.distance * 100
             # Print distance for debugging
             print('Distance: ', distance)
-            # Take the picture and trigger the flash if something is close enough
-            if distance < 10:
-                # Take the picture and trigger the flash
-                take_picture_and_flash()
-                # Break out of loop
-                break
+            # Take the picture if something is close enough and there is motion detected
+            if distance < 10 and pir.motion_detected:
+                # Take the picture
+                take_picture()
             # Wait for 1 second before checking again
-            time.sleep(0.5)
+            time.sleep(1)
     except KeyboardInterrupt:
         print("Program terminated by user")
     finally:
@@ -51,4 +42,3 @@ if __name__ == '__main__':
         pir.close()
         ultrasonic.close()
         led.close()
-        flash.close()
