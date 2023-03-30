e5e1ae400959a1577fdf683162b433f22fac1826
matthaeus123 <60934578+matthaeus123@users.noreply.github.com>
2023-03-29 18:14:12 -0700

    Update debugmotion.py

--- debugmotion.py
+++ debugmotion.py
@@ -4,44 +4,35 @@ import pigpio
 # Set up the motion sensor on GPIO pin 6
 pir_pin = 6
 pi = pigpio.pi()
+pi.set_mode(pir_pin, pigpio.INPUT)
+pi.set_pull_up_down(pir_pin, pigpio.PUD_DOWN)
 
+# Check whether the pin is a PWM pin and print the sensitivity if available
 try:
-    pi.set_mode(pir_pin, pigpio.INPUT)
-    pi.set_pull_up_down(pir_pin, pigpio.PUD_DOWN)
-
-    # Check whether the pin is a PWM pin
     if pi.get_PWM_range(pir_pin) != 0:
-        # Get the current sensitivity of the motion sensor
         sensitivity = pi.get_PWM_dutycycle(pir_pin)
         print(f"Sensitivity: {sensitivity}")
-    else:
-        print("PWM not supported on this pin")
+except:
+    print("Error reading sensitivity")
 
-    # Get the current delay and trigger mode of the motion sensor
+# Get the current delay and trigger mode of the motion sensor
+try:
     delay = pi.get_mode(pir_pin)
     trigger = pi.get_pull_up_down(pir_pin)
     print(f"Delay: {delay}, Trigger mode: {trigger}")
+except:
+    print("Error reading delay and trigger mode")
 
-    # Loop indefinitely
-    while True:
-        # Read the current state of the motion sensor
-        try:
-            motion_detected = pi.read(pir_pin)
+# Loop indefinitely
+while True:
+    # Read the current state of the motion sensor
+    motion_detected = pi.read(pir_pin)
 
-            # Print the state of the motion sensor
-            if motion_detected:
-                print("Motion detected!")
-            else:
-                print("No motion detected.")
+    # Print the state of the motion sensor
+    if motion_detected:
+        print("Motion detected!")
+    else:
+        print("No motion detected.")
 
-        except pigpio.error as e:
-            print(f"Error reading motion sensor: {e}")
-
-        # Wait for a moment before checking again
-        time.sleep(0.1)
-
-except KeyboardInterrupt:
-    print("Interrupted by user")
-
-finally:
-    pi.stop()
+    # Wait for a moment before checking again
+    time.sleep(0.1)
