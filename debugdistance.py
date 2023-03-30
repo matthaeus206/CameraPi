51c1036d637e4c6f63f66d29d228a082198afca0
matthaeus123 <60934578+matthaeus123@users.noreply.github.com>
2023-03-29 17:35:14 -0700

    Update debugdistance.py

--- debugdistance.py
+++ debugdistance.py
@@ -1,8 +1,44 @@
-from gpiozero import DistanceSensor
 import time
+import pigpio
 
-sensor = DistanceSensor(echo=4, trigger=5)
+# Initialize the pigpio library and the GPIO pins
+pi = pigpio.pi()
+trigger_pin = 5
+echo_pin = 4
+
+# Set the trigger pin as an output and the echo pin as an input
+pi.set_mode(trigger_pin, pigpio.OUTPUT)
+pi.set_mode(echo_pin, pigpio.INPUT)
+
+# Set the trigger pin to low (false) initially
+pi.write(trigger_pin, 0)
 
 while True:
-    print("Distance: {:.2f} cm".format(sensor.distance * 100))
-    time.sleep(0.5)
+    # Send a 10us pulse to the trigger pin to start the measurement
+    pi.write(trigger_pin, 1)
+    time.sleep(0.00001)
+    pi.write(trigger_pin, 0)
+
+    # Wait for the echo pin to go high
+    start_time = time.time()
+    while pi.read(echo_pin) == 0:
+        if (time.time() - start_time) > 1:
+            print("Echo pin is not detecting any signal")
+            break
+
+    # Wait for the echo pin to go low
+    end_time = time.time()
+    while pi.read(echo_pin) == 1:
+        if (time.time() - end_time) > 1:
+            print("Echo pin is not going low after being triggered")
+            break
+
+    # Calculate the duration of the pulse and convert it to distance in cm
+    pulse_duration = end_time - start_time
+    distance = pulse_duration * 17150
+
+    # Print the distance
+    print("Distance: %.1f cm" % distance)
+
+    # Wait for one second before taking the next measurement
+    time.sleep(1)
