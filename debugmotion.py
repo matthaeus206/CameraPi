91a370f85229cf506d16272ec323e885355d6dad
matthaeus123 <60934578+matthaeus123@users.noreply.github.com>
2023-03-29 18:17:13 -0700

    Update debugmotion.py

--- debugmotion.py
+++ debugmotion.py
@@ -7,22 +7,6 @@ pi = pigpio.pi()
 pi.set_mode(pir_pin, pigpio.INPUT)
 pi.set_pull_up_down(pir_pin, pigpio.PUD_DOWN)
 
-# Check whether the pin is a PWM pin and print the sensitivity if available
-try:
-    if pi.get_PWM_range(pir_pin) != 0:
-        sensitivity = pi.get_PWM_dutycycle(pir_pin)
-        print(f"Sensitivity: {sensitivity}")
-except:
-    print("Error reading sensitivity")
-
-# Get the current delay and trigger mode of the motion sensor
-try:
-    delay = pi.get_mode(pir_pin)
-    trigger = pi.get_pull_up_down(pir_pin)
-    print(f"Delay: {delay}, Trigger mode: {trigger}")
-except:
-    print("Error reading delay and trigger mode")
-
 # Loop indefinitely
 while True:
     # Read the current state of the motion sensor
