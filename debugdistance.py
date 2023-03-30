ac643b1947bcff24c9ef6b6bbf9d1f5a7a777794
matthaeus123 <60934578+matthaeus123@users.noreply.github.com>
2023-03-29 17:32:40 -0700

    Update debugdistance.py

--- debugdistance.py
+++ debugdistance.py
@@ -1,28 +1,8 @@
-import time
-import pigpio
 from gpiozero import DistanceSensor
+import time
 
-# Initialize the distance sensor
-ultrasonic = DistanceSensor(echo=4, trigger=5)
+sensor = DistanceSensor(echo=4, trigger=5)
 
-# Initialize pigpio
-pi = pigpio.pi()
-
-# Define a callback function to print the distance of the object
-def cb_echo(gpio, level, tick):
-    distance = ultrasonic.distance * 100 # convert to centimeters
-    print(f"Distance: {distance:.2f} cm")
-
-# Add a callback to the echo pin
-cb = pi.callback(ultrasonic.echo_pin, pigpio.EITHER_EDGE, cb_echo)
-
-try:
-    # Keep the script running indefinitely
-    while True:
-        time.sleep(1)
-
-except KeyboardInterrupt:
-    # Clean up resources on keyboard interrupt
-    cb.cancel()
-    pi.stop()
-    ultrasonic.close()
+while True:
+    print("Distance: {:.2f} cm".format(sensor.distance * 100))
+    time.sleep(0.5)
