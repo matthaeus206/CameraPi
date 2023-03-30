ca4a3ab99fbc6b6fa6a6214e98ccd6077ab8b4ba
matthaeus123 <60934578+matthaeus123@users.noreply.github.com>
2023-03-30 15:57:58 -0700

    Update debugpower.py

--- debug/debugpower.py
+++ debug/debugpower.py
@@ -1,12 +1,24 @@
+import subprocess
 import time
 
 while True:
-    # Check for lightning bolt symbol
-    if open('/sys/devices/platform/soc/soc:firmware/get_throttled').read().split()[0] != '0':
-        print("Lightning bolt symbol detected")
+    # Check for undervoltage
+    voltage_output = subprocess.check_output(['vcgencmd', 'measure_volts', 'core'])
+    voltage = float(voltage_output.decode().split('=')[1].split('V')[0])
+    if voltage < 4.65:
+        print("Warning: undervoltage detected!")
 
-    # Check for instability or crashes
-    print("If the Raspberry Pi is freezing or crashing frequently, it may be a sign of undervoltage.")
+    # Check for overtemperature
+    temp_output = subprocess.check_output(['vcgencmd', 'measure_temp'])
+    temp = float(temp_output.decode().split('=')[1].split("'")[0])
+    if temp > 80.0:
+        print("Warning: overtemperature detected!")
+
+    # Check for high CPU usage
+    cpu_output = subprocess.check_output(['top', '-n', '1', '-b'])
+    cpu_usage = float(cpu_output.decode().split('Cpu(s):')[1].split('%')[0])
+    if cpu_usage > 90.0:
+        print("Warning: high CPU usage detected!")
 
     # Wait 10 seconds before checking again
     time.sleep(10)
