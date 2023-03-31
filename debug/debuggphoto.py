063ce640a3d410c55c58212a8d60af6c2117ab25
matthaeus123 <60934578+matthaeus123@users.noreply.github.com>
2023-03-31 16:02:05 -0700

    Update debuggphoto.py

--- debug/debuggphoto.py
+++ debug/debuggphoto.py
@@ -1,24 +1,9 @@
-import subprocess
+import os
 import time
 from gpiozero import MotionSensor
 
-# Initialize the motion sensor
-pir = MotionSensor(6)
-
-# Set the gphoto2 configuration to save photos on the camera's memory card
-subprocess.call(["gphoto2", "--set-config", "capturetarget=1"])
-
-# Continuously check for motion and take photos when detected
 while True:
+    pir = MotionSensor(6)
     if pir.motion_detected:
-        # Print a message when motion is detected
-        print("Motion detected!")
-
-        # Take a photo and save it on the camera's memory card
-        subprocess.call(["gphoto2", "--trigger-capture"])
-        
-        # Print a message when the photo is captured
-        print("Photo captured!")
-        
-        # Wait for 1 second before checking for motion again
-        time.sleep(1)
+        os.system("gphoto2 --capture-image-and-download --filename /store_00010001/DCIM/100CANON/%Y%m%d-%H%M%S.%C")
+    time.sleep(1)
