bdae5fc656a9fa378e9c741545963cb0d8a31b70
matthaeus123 <60934578+matthaeus123@users.noreply.github.com>
2023-03-31 16:04:33 -0700

    Update debuggphoto.py

--- debug/debuggphoto.py
+++ debug/debuggphoto.py
@@ -1,9 +1,11 @@
 import os
-import time
 from gpiozero import MotionSensor
+import time
+
+pir = MotionSensor(6)
 
 while True:
-    pir = MotionSensor(6)
     if pir.motion_detected:
-        os.system("gphoto2 --capture-image-and-download --filename /store_00010001/DCIM/100CANON/%Y%m%d-%H%M%S.%C")
-    time.sleep(1)
+        print("Motion detected")
+        os.system("gphoto2 --capture-image-and-download --filename '/store_00010001/DCIM/100CANON/%Y%m%d-%H%M%S.%C'")
+    time.sleep(5)
