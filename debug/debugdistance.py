e25cfb8f0c244fbc687e5cc579976b8b6b0980bf
matthaeus123 <60934578+matthaeus123@users.noreply.github.com>
2023-03-31 12:01:59 -0700

    Update debugdistance.py

--- debug/debugdistance.py
+++ debug/debugdistance.py
@@ -1,26 +1,38 @@
 import RPi.GPIO as GPIO
 import time
 
-# Set up HC-SR501 motion sensor
-pir_pin = 18
+# Set up HC-SR04 distance sensor
+trig_pin = 4
+echo_pin = 5
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(pir_pin, GPIO.IN)
+GPIO.setup(trig_pin, GPIO.OUT)
+GPIO.setup(echo_pin, GPIO.IN)
 
-# Set up distance sensor
-# Replace the following lines with the appropriate code for your specific sensor
-distance_pin = 23
-GPIO.setup(distance_pin, GPIO.IN)
+# Send a pulse to trigger the sensor and measure the time it takes to receive the echo
+def get_distance():
+    # Send a 10us pulse to trigger the sensor
+    GPIO.output(trig_pin, True)
+    time.sleep(0.00001)
+    GPIO.output(trig_pin, False)
+
+    # Wait for the echo to start and stop, and measure the duration
+    pulse_start = time.time()
+    while GPIO.input(echo_pin) == 0:
+        pulse_start = time.time()
+
+    pulse_end = time.time()
+    while GPIO.input(echo_pin) == 1:
+        pulse_end = time.time()
+
+    pulse_duration = pulse_end - pulse_start
+
+    # Convert the duration to distance in cm
+    distance = pulse_duration * 17150
+    distance = round(distance, 2)
+
+    return distance
 
 while True:
-    # Check for motion with HC-SR501
-    if GPIO.input(pir_pin):
-        print("Motion detected")
-
-    # Read distance from distance sensor
-    # Replace the following line with the appropriate code for your specific sensor
-    distance = GPIO.input(distance_pin)
-
-    # Uncomment the following line if you want to print distance as well
-    # print("Distance: {} cm".format(distance))
-
+    distance = get_distance()
+    print("Distance: {} cm".format(distance))
     time.sleep(0.1)
