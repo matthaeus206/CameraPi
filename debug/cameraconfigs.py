a64b83b21e69ca3d1dddbd41066944ebadc2e3cd
matthaeus123 <60934578+matthaeus123@users.noreply.github.com>
2023-04-02 14:05:07 -0700

    Update cameraconfigs.py

--- debug/cameraconfigs.py
+++ debug/cameraconfigs.py
@@ -1,23 +1,32 @@
 import subprocess
 
-# Run gphoto2 with the --list-config option to get a list of all available configuration options
-output = subprocess.check_output(["gphoto2", "--list-config"])
+# Define the configuration options we want to get the current values for
+config_options = [
+    "/main/capturesettings/capturemode",
+    "/main/imgsettings/iso",
+    "/main/capturesettings/shutterspeed",
+    "/main/capturesettings/aperture"
+]
 
-# Split the output into individual lines
-lines = output.decode('utf-8').split("\n")
+try:
+    # Iterate over each configuration option and print its current value
+    for config_option in config_options:
+        try:
+            # Use the --get-config-value option to get the current value of the configuration option
+            value = subprocess.check_output(["gphoto2", "--get-config-value", config_option]).decode('utf-8').strip()
+        except subprocess.CalledProcessError:
+            value = "N/A"
+        print(config_option + ": " + value)
 
-# Define a list of configuration groups to include
-config_groups = ["iso", "shutterspeed", "aperture", "imagequality", "whitebalance"]
-
-# Iterate over each line and check if it belongs to a relevant configuration group
-for line in lines:
-    if line.startswith("/"):
-        config_option = line.strip()
-        config_group = config_option.split("/")[-2]
-        if config_group in config_groups:
+    # Write the output to a text file in the same directory as the script
+    with open("camera_settings.txt", "w") as f:
+        for config_option in config_options:
             try:
-                # Use the --get-config-value option to get the current value of the configuration option
                 value = subprocess.check_output(["gphoto2", "--get-config-value", config_option]).decode('utf-8').strip()
             except subprocess.CalledProcessError:
                 value = "N/A"
-            print(config_option + ": " + value)
+            f.write(config_option + ": " + value + "\n")
+
+    print("Settings written to camera_settings.txt")
+except Exception as e:
+    print("Error: " + str(e))
