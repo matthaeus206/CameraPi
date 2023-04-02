3b232acecea81b0264284cba9f5cb236d8ded683
matthaeus123 <60934578+matthaeus123@users.noreply.github.com>
2023-04-02 14:00:11 -0700

    Update cameraconfigs.py

--- debug/cameraconfigs.py
+++ debug/cameraconfigs.py
@@ -6,18 +6,23 @@ output = subprocess.check_output(["gphoto2", "--list-config"])
 # Split the output into individual lines
 lines = output.decode('utf-8').split("\n")
 
-# Create a new text file in the same folder as the script
-with open("camera_config.txt", "w") as file:
+# Define the configuration options we are interested in
+iso_config = "/main/capturesettings/iso"
+shutter_speed_config = "/main/capturesettings/shutterspeed"
 
-    # Iterate over each line and print the current configuration value for each option
-    for line in lines:
-        if line.startswith("/"):
-            config_option = line.strip()
-            try:
-                # Use the --get-config-value option to get the current value of the configuration option
-                value = subprocess.check_output(["gphoto2", "--get-config-value", config_option]).decode('utf-8').strip()
-            except subprocess.CalledProcessError:
-                value = "N/A"
-            output_line = config_option + ": " + value + "\n"
-            file.write(output_line)
-            print(output_line)
+# Iterate over each line and print the current value for the ISO and shutter speed options
+for line in lines:
+    if line.startswith(iso_config):
+        try:
+            # Use the --get-config-value option to get the current value of the ISO option
+            iso_value = subprocess.check_output(["gphoto2", "--get-config-value", iso_config]).decode('utf-8').strip()
+        except subprocess.CalledProcessError:
+            iso_value = "N/A"
+        print("Current ISO: " + iso_value)
+    elif line.startswith(shutter_speed_config):
+        try:
+            # Use the --get-config-value option to get the current value of the shutter speed option
+            shutter_speed_value = subprocess.check_output(["gphoto2", "--get-config-value", shutter_speed_config]).decode('utf-8').strip()
+        except subprocess.CalledProcessError:
+            shutter_speed_value = "N/A"
+        print("Current Shutter Speed: " + shutter_speed_value)
