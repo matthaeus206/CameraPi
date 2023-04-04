51e24920771bf050ce1422c6482cdc830004dfac
matthaeus123 <60934578+matthaeus123@users.noreply.github.com>
2023-04-03 17:49:26 -0700

    Update CaptureWithFlash.py

--- CaptureWithFlash.py
+++ CaptureWithFlash.py
@@ -1,7 +1,7 @@
-import subprocess
-import time
 import pigpio
 from gpiozero import MotionSensor, LED
+import subprocess
+import time
 
 # Initialize the pigpio instance
 pi = pigpio.pi()
@@ -16,55 +16,32 @@ led = LED(17)
 flash_pin = 18
 pi.set_mode(flash_pin, pigpio.OUTPUT)
 
-# Define function to take picture and trigger flash
-def take_picture():
+try:
     while True:
-        # Wait for motion to be detected
         pir.wait_for_motion()
-
-        # Turn on the LED
+        print("Motion detected")
         led.on()
-
-        # Try to set autofocus mode to single-shot
+        
+        # Take a photo
         try:
-            subprocess.check_call(["gphoto2", "--set-config", "/main/capturesettings/afmode=0"])
+            subprocess.check_call(["gphoto2", "--set-config", "eosremoterelease=Immediate"])
+            subprocess.check_call(["gphoto2", "--set-config", "eosremoterelease=Release Full"])
+            subprocess.check_call(["gphoto2", "--wait-event-and-download=FILEADDED"])
+            print("Photo taken")
         except subprocess.CalledProcessError as e:
-            print("Could not set autofocus mode:", e)
-
-        # Try to start autofocus
-        try:
-            subprocess.check_call(["gphoto2", "--set-config", "/main/actions/autofocus=1"])
-        except subprocess.CalledProcessError as e:
-            print("Could not start autofocus:", e)
-
-        # Wait for autofocus to lock
-        print("Waiting for autofocus to lock...")
-        subprocess.call(["gphoto2", "--wait-event", "5s", "--event-to-stdout", "--quiet"])
-
-        # Press the shutter button fully to take the picture
-        try:
-            subprocess.check_call(["gphoto2", "--set-config", "/main/actions/eosremoterelease=4"])
-        except subprocess.CalledProcessError as e:
-            print("Could not press shutter button:", e)
-
-        # Wait for the camera to start exposure
-        time.sleep(0.01)
-
+            print("Could not take photo:", e)
+            
         # Trigger the flash
         pi.write(flash_pin, 1)
         time.sleep(0.01)
         pi.write(flash_pin, 0)
 
-        # Turn off the LED
+        pir.wait_for_no_motion()
+        print("Motion stopped")
         led.off()
 
-if __name__ == '__main__':
-    try:
-        # Call the take_picture function
-        take_picture()
-    except KeyboardInterrupt:
-        # Clean up GPIO resources on keyboard interrupt
-        print("\nCleaning up GPIO resources...")
-        pir.close()
-        led.close()
-        pi.stop()
+finally:
+    # Clean up resources
+    pir.close()
+    led.close()
+    pi.stop()
