d3031b36e6f642735b77bd35c047390d4b36c170
matthaeus123 <60934578+matthaeus123@users.noreply.github.com>
2023-04-03 18:00:37 -0700

    Update CaptureWithFlash.py

--- CaptureWithFlash.py
+++ CaptureWithFlash.py
@@ -3,7 +3,7 @@ import time
 import pigpio
 from gpiozero import MotionSensor, LED
 
-# Set up camera
+# Set up the camera
 subprocess.call(["gphoto2", "--set-config", "eosremoterelease=Immediate"])
 subprocess.call(["gphoto2", "--set-config", "eosremoterelease=Release Full"])
 
@@ -20,46 +20,36 @@ led = LED(17)
 flash_pin = 18
 pi.set_mode(flash_pin, pigpio.OUTPUT)
 
+# Wait for 10 seconds before starting the loop
+print("Waiting for 10 seconds...")
+time.sleep(10)
+
+# Start the loop
+print("Starting the loop...")
 while True:
-    # Wait for motion to be detected
-    pir.wait_for_motion()
+    if pir.motion_detected:
+        # Turn on the LED
+        led.on()
 
-    # Turn on the LED
-    led.on()
-
-    # Trigger the flash
-    pi.write(flash_pin, 1)
-    time.sleep(0.01)
-    pi.write(flash_pin, 0)
-
-    # Take a picture
-    try:
-        subprocess.check_call(["gphoto2", "--capture-image-and-download", "--force-overwrite"])
-        print("Photo taken")
-    except subprocess.CalledProcessError as e:
-        print("Could not take photo:", e)
-
-    # Wait for motion to stop
-    pir.wait_for_no_motion()
-
-    # Turn off the LED
-    led.off()
-
-    # Keep taking pictures while motion is still detected
-    while pir.motion_detected:
         # Trigger the flash
         pi.write(flash_pin, 1)
         time.sleep(0.01)
         pi.write(flash_pin, 0)
 
-        # Take a picture
+        # Take a photo
+        print("Taking photo...")
         try:
-            subprocess.check_call(["gphoto2", "--capture-image-and-download", "--force-overwrite"])
+            subprocess.check_call(["gphoto2", "--capture-image-and-download"])
             print("Photo taken")
         except subprocess.CalledProcessError as e:
             print("Could not take photo:", e)
 
-# Clean up resources
-pir.close()
-led.close()
-pi.stop()
+        # Wait for 5 seconds before checking for motion again
+        time.sleep(5)
+
+    else:
+        # Turn off the LED
+        led.off()
+
+        # Wait for 1 second before checking for motion again
+        time.sleep(1)
