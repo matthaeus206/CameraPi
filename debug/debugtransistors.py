aef9b6bf677f714c3ab7eaa333d4813e18d477fb
Matt <60934578+matthaeus206@users.noreply.github.com>
2023-04-09 18:20:26 -0700

    Update debugtransistors.py
    updated it to test all transistors

--- debug/debugtransistors.py
+++ debug/debugtransistors.py
@@ -1,17 +1,44 @@
 import RPi.GPIO as GPIO
 import time
 
-# Set up GPIO pin
-transistor_pin = 18
+# Set up GPIO pins
+flash_pin = 18
+half_press_pin = 23
+full_press_pin = 24
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(transistor_pin, GPIO.OUT)
+GPIO.setup(flash_pin, GPIO.OUT)
+GPIO.setup(half_press_pin, GPIO.OUT)
+GPIO.setup(full_press_pin, GPIO.OUT)
 
 try:
-    # Trigger transistor for 1 second
-    print("Triggering transistor for 1 second...")
-    GPIO.output(transistor_pin, True)
-    time.sleep(1)
-    GPIO.output(transistor_pin, False)
-    print("Transistor test complete!")
+    # Display menu prompt
+    print("Which transistor do you want to test?")
+    print("1. Flash transistor")
+    print("2. Half press transistor")
+    print("3. Full press transistor")
+    choice = int(input("Enter your choice (1-3): "))
+
+    # Test selected transistor
+    if choice == 1:
+        print("Testing flash transistor...")
+        GPIO.output(flash_pin, True)
+        time.sleep(0.5)
+        GPIO.output(flash_pin, False)
+        print("Flash transistor test complete!")
+    elif choice == 2:
+        print("Testing half press transistor...")
+        GPIO.output(half_press_pin, True)
+        time.sleep(0.5)
+        GPIO.output(half_press_pin, False)
+        print("Half press transistor test complete!")
+    elif choice == 3:
+        print("Testing full press transistor...")
+        GPIO.output(full_press_pin, True)
+        time.sleep(0.5)
+        GPIO.output(full_press_pin, False)
+        print("Full press transistor test complete!")
+    else:
+        print("Invalid choice. Please enter a number between 1 and 3.")
+
 except KeyboardInterrupt:
     GPIO.cleanup()
