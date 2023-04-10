f049d0009bb377cf69735030a6e30a77675ee99b
Matt <60934578+matthaeus206@users.noreply.github.com>
2023-04-09 18:27:52 -0700

    Create debug.py

---
+++ debug/debug.py
@@ -0,0 +1,30 @@
+import os
+
+while True:
+    # Display menu prompt
+    print("Which debug script do you want to run?")
+    print("1. Test current camera configs")
+    print("2. Test ultrasonic distance sensor")
+    print("3. Test camera")
+    print("4. Test HC-SR501 motion sensor")
+    print("5. Test undervoltage")
+    print("6. Test camera trigger")
+    print("0. Exit")
+
+    choice = input("Enter your choice (0-6): ")
+
+    # Run selected debug script
+    if choice == "1":
+        os.system("python3 cameraconfigs.py")
+    elif choice == "2":
+        os.system("python3 debugdistance.py")
+    elif choice == "3":
+        os.system("python3 debuggphoto.py")
+    elif choice == "4":
+        os.system("python3 debugmotion.py")
+    elif choice == "5":
+        os.system("python3 debugpower.py")
+    elif choice == "6":
+        os.system("python3 debugtransistors.py")
+    elif choice == "0":
+        break
