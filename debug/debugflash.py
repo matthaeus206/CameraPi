047f77b4d976acc0f021a42bac0ac5d489442099
Matt <60934578+matthaeus206@users.noreply.github.com>
2023-04-24 16:06:03 -0700

    Update debugflash.py

--- debug/debugflash.py
+++ debug/debugflash.py
@@ -2,10 +2,10 @@ import RPi.GPIO as GPIO
 import time
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(4, GPIO.OUT)
+GPIO.setup(5, GPIO.OUT)
 
 while True:
-    GPIO.output(4, GPIO.HIGH)
+    GPIO.output(5, GPIO.HIGH)
     time.sleep(5)
-    GPIO.output(4, GPIO.LOW)
+    GPIO.output(5, GPIO.LOW)
     time.sleep(5)
