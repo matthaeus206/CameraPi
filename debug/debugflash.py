a37f8e834156e20ea2e1d787d17e6ef3d7a0cf17
Matt <60934578+matthaeus206@users.noreply.github.com>
2023-04-24 19:03:56 -0700

    Update debugflash.py

--- debug/debugflash.py
+++ debug/debugflash.py
@@ -2,10 +2,16 @@ import RPi.GPIO as GPIO
 import time
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(5, GPIO.OUT)
+GPIO.setup(5, GPIO.OUT)  # set up GPIO pin 5 as an output for the signal to the transistor base
 
 while True:
     GPIO.output(5, GPIO.HIGH)
-    time.sleep(5)
+    time.sleep(0.1)  # adjust this delay as necessary for your camera
     GPIO.output(5, GPIO.LOW)
-    time.sleep(5)
+    time.sleep(0.1)  # adjust this delay as necessary for your camera
+
+    # Trigger the camera by applying a signal to the base of the transistor on GPIO pin 5
+    GPIO.output(5, GPIO.HIGH)
+    time.sleep(0.1)  # adjust this delay as necessary for your camera
+    GPIO.output(5, GPIO.LOW)
+    time.sleep(5)  # adjust this delay as necessary for your camera
