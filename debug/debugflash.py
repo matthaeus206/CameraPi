579548e1922ac1b8798e344a819f286a3bf79331
Matt <60934578+matthaeus206@users.noreply.github.com>
2023-04-24 19:08:29 -0700

    Update debugflash.py

--- debug/debugflash.py
+++ debug/debugflash.py
@@ -4,14 +4,19 @@ import time
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(4, GPIO.OUT)  # set up GPIO pin 4 as an output for the signal to the transistor base
 
-while True:
-    GPIO.output(5, GPIO.HIGH)
-    time.sleep(0.1)  # adjust this delay as necessary for your camera
-    GPIO.output(4, GPIO.LOW)
-    time.sleep(0.1)  # adjust this delay as necessary for your camera
+try:
+    while True:
+        GPIO.output(4, GPIO.HIGH)
+        time.sleep(0.1)  # adjust this delay as necessary for your camera
+        GPIO.output(4, GPIO.LOW)
+        time.sleep(0.1)  # adjust this delay as necessary for your camera
 
-    # Trigger the camera by applying a signal to the base of the transistor on GPIO pin 5
-    GPIO.output(4, GPIO.HIGH)
-    time.sleep(0.1)  # adjust this delay as necessary for your camera
-    GPIO.output(4, GPIO.LOW)
-    time.sleep(5)  # adjust this delay as necessary for your camera
+        # Trigger the camera by applying a signal to the base of the transistor on GPIO pin 4
+        GPIO.output(4, GPIO.HIGH)
+        time.sleep(0.1)  # adjust this delay as necessary for your camera
+        GPIO.output(4, GPIO.LOW)
+        time.sleep(5)  # adjust this delay as necessary for your camera
+
+except KeyboardInterrupt:
+    # Clean up GPIO pin 4 and exit gracefully on Ctrl+C
+    GPIO.cleanup(4)
