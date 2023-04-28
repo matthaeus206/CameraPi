1bb4dbf4e804304228db63c95cc5c4c422c51e6f
Matt <60934578+matthaeus206@users.noreply.github.com>
2023-04-28 14:13:24 -0700

    Update debugflash.py

--- debug/debugflash.py
+++ debug/debugflash.py
@@ -5,18 +5,18 @@ import time
 GPIO.setmode(GPIO.BCM)
 
 # Set pin 5 as an output
-GPIO.setup(5, GPIO.OUT)
+GPIO.setup(4, GPIO.OUT)
 
 # Loop forever
 while True:
     # Turn on pin 5
-    GPIO.output(5, GPIO.HIGH)
+    GPIO.output(4, GPIO.HIGH)
 
     # Wait for 2 seconds
     time.sleep(2)
 
     # Turn off pin 5
-    GPIO.output(5, GPIO.LOW)
+    GPIO.output(4, GPIO.LOW)
 
     # Wait for 2 seconds
     time.sleep(2)
