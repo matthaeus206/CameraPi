21c548b45e3cf2fe7974e45db3f72e9240941da3
Matt <60934578+matthaeus206@users.noreply.github.com>
2023-04-28 14:32:01 -0700

    Update debugflash.py

--- debug/debugflash.py
+++ debug/debugflash.py
@@ -4,19 +4,22 @@ import time
 # Set the pin numbering scheme to use the BCM scheme
 GPIO.setmode(GPIO.BCM)
 
-# Set pin 5 as an output
+# Set pin 4 as an output
 GPIO.setup(4, GPIO.OUT)
 
 # Loop forever
 while True:
-    # Turn on pin 5
+    # Turn on pin 4
     GPIO.output(4, GPIO.HIGH)
 
     # Wait for 2 seconds
     time.sleep(2)
 
-    # Turn off pin 5
+    # Turn off pin 4
     GPIO.output(4, GPIO.LOW)
 
     # Wait for 2 seconds
     time.sleep(2)
+
+# Clean up the GPIO pins
+GPIO.cleanup()
