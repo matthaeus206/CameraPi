e3d11a90c28ff31afa405f3b587853904a24b706
Matt <60934578+matthaeus206@users.noreply.github.com>
2023-04-30 13:20:26 -0700

    Update debugmotion.py

--- debug/debugmotion.py
+++ debug/debugmotion.py
@@ -2,12 +2,12 @@ import RPi.GPIO as GPIO
 import time
 import subprocess
 
-# Set up HC-SR501 motion sensor
-pir_pin = 6
+# Set up GPIO mode and pin for HC-SR501 motion sensor
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(pir_pin, GPIO.IN)
+PIR_PIN = 6
+GPIO.setup(PIR_PIN, GPIO.IN)
 
-# Set up a function to check for undervoltage
+# Set up a function to check for undervoltage using subprocess module
 def check_undervoltage():
     # Run vcgencmd to check the undervoltage status
     result = subprocess.check_output("vcgencmd get_throttled", shell=True)
@@ -15,13 +15,15 @@ def check_undervoltage():
     # Check if the "undervoltage" bit is set in the result
     return "0x50000" in result.decode("utf-8")
 
+# Main loop to check for motion and undervoltage
 while True:
     # Check for motion with HC-SR501
-    if GPIO.input(pir_pin):
+    if GPIO.input(PIR_PIN):
         print("Motion detected")
 
-    # Check for undervoltage
+    # Check for undervoltage with the check_undervoltage function
     if check_undervoltage():
         print("Undervoltage detected")
 
-    time.sleep(1)
+    # Wait for 1.5 seconds before checking the sensor again
+    time.sleep(1.5)
