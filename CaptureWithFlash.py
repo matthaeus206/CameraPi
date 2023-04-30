1b3eab6aa8df8753acd7ab2de5531d60377d5540
Matt <60934578+matthaeus206@users.noreply.github.com>
2023-04-30 15:33:20 -0700

    Update CaptureWithFlash.py
    rewrite

--- CaptureWithFlash.py
+++ CaptureWithFlash.py
@@ -1,55 +1,34 @@
-import pigpio
-from gpiozero import MotionSensor, LED
-import subprocess
-import time
+# Import necessary modules
+import RPi.GPIO as GPIO
+from gpiozero import MotionSensor, DistanceSensor
+from time import sleep
 
-# Initialize the pigpio instance
-pi = pigpio.pi()
+# Set up GPIO numbering mode
+GPIO.setmode(GPIO.BCM)
 
-# Initialize the motion sensor
+# Set up GPIO pins for the transistors
+flash_pin = 4
+shutter_pin = 5
+GPIO.setup(flash_pin, GPIO.OUT)
+GPIO.setup(shutter_pin, GPIO.OUT)
+
+# Initialize the motion and distance sensors
 pir = MotionSensor(6)
+ultrasonic = DistanceSensor(echo=16, trigger=17, max_distance=100, queue_len=2)
 
-# Initialize the LED
-led = LED(17)
+# Define a function to trigger the flash and shutter release
+def trigger_camera():
+    # Trigger flash
+    GPIO.output(flash_pin, GPIO.HIGH)
+    sleep(1.2)
+    GPIO.output(flash_pin, GPIO.LOW)
+    # Trigger shutter release
+    GPIO.output(shutter_pin, GPIO.HIGH)
+    sleep(0.1)
+    GPIO.output(shutter_pin, GPIO.LOW)
 
-# Initialize the GPIO pin for the flash
-flash_pin = 18
-pi.set_mode(flash_pin, pigpio.OUTPUT)
-
-# Wait for motion
-pir.wait_for_motion()
-print("Motion detected")
-led.on()
-
-try:
-    while True:
-        # Take a photo if motion is detected
-        if pir.motion_detected:
-            try:
-                subprocess.check_call(["gphoto2", "--set-config", "eosremoterelease=Immediate"])
-                subprocess.check_call(["gphoto2", "--set-config", "eosremoterelease=Release Full"])
-                subprocess.check_call(["gphoto2", "--wait-event-and-download=FILEADDED", "--raw", "--force-overwrite"])
-                print("Photo taken")
-            except subprocess.CalledProcessError as e:
-                print("Could not take photo:", e)
-                
-            # Trigger the flash
-            pi.write(flash_pin, 1)
-            time.sleep(0.01)
-            pi.write(flash_pin, 0)
-
-        # Wait for motion to stop
-        pir.wait_for_no_motion()
-        print("Motion stopped")
-        led.off()
-
-        # Wait for motion to start again
-        pir.wait_for_motion()
-        print("Motion detected")
-        led.on()
-
-finally:
-    # Clean up resources
-    pir.close()
-    led.close()
-    pi.stop()
+# Continuously monitor the sensors and trigger the transistors when conditions are met
+while True:
+    if pir.motion_detected and ultrasonic.distance > 0.254:  # 10 inches in meters
+        trigger_camera()
+    sleep(0.1)  # Sleep for 100ms between sensor readings
