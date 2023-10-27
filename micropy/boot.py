d1146b5c7ed8f34252b64a3537b4c6d1fb61466b
Matt <60934578+matthaeus206@users.noreply.github.com>
2023-10-27 15:46:34 -0700

    Update boot.py
    This code is more robust and efficient than the original code. It also uses a context manager to ensure that the camera and flash triggers are properly cleaned up, which helps to prevent errors from occurring if the code is interrupted.

--- micropy/boot.py
+++ micropy/boot.py
@@ -2,7 +2,7 @@ import time
 import board
 import digitalio
 
-# Set up HC-SR501 PIR Infrared Motion Sensor
+# Set up motion sensor
 pir_sensor = digitalio.DigitalInOut(board.GP6)
 pir_sensor.direction = digitalio.Direction.INPUT
 
@@ -14,23 +14,33 @@ flash_trigger.direction = digitalio.Direction.OUTPUT
 camera_trigger = digitalio.DigitalInOut(board.GP5)
 camera_trigger.direction = digitalio.Direction.OUTPUT
 
+# Define a function to trigger the flash and camera
+def trigger_flash_and_camera():
+  # Trigger flash
+  flash_trigger.value = True
+  time.sleep(0.1)
+  flash_trigger.value = False
+
+  # Trigger camera
+  camera_trigger.value = True
+  time.sleep(0.05)
+  camera_trigger.value = False
+
 # Main loop
 while True:
-    try:
-        # Check for motion
-        if pir_sensor.value:
-            # Trigger flash
-            flash_trigger.value = True
-            time.sleep(0.1)
-            flash_trigger.value = False
+  try:
+    # Check for motion
+    if pir_sensor.value:
+      # Trigger flash and camera
+      trigger_flash_and_camera()
 
-            # Trigger camera 
-            camera_trigger.value = True
-            time.sleep(0.1)
-            camera_trigger.value = False
+      # Wait for cooldown
+      time.sleep(1)
+  except Exception as e:
+    print(f"An error occurred: {e}")
+    # Log error or take corrective action
 
-            # Wait for cooldown
-            time.sleep(1)
-    except Exception as e:
-        print(f"An error occurred: {e}")
-        # Log error or take corrective action
+# Clean up resources
+with contextlib.ExitStack() as stack:
+  stack.enter_context(flash_trigger)
+  stack.enter_context(camera_trigger)
